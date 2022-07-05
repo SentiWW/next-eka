5374739cf5a4e58b0a492bac420484b6c59827ef
Piotr Szczypior <piotrekszczypior1@gmail.com>
2022-07-05 23:40:01 +0200

    contact us

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,5 +1,12 @@
 import ComponentWrapper from "../ComponentWrapper";
 import SectionHeader from "../SectionHeader";
+import facebookIcon from "./../../public/icons/facebook.svg";
+import envelopeIcon from "./../../public/icons/envelope.svg";
+import twitterIcon from "./../../public/icons/twitter.svg";
+import linkedinIcon from "./../../public/icons/linkedin.svg";
+
+
+import style from "./Contact.module.css";
 
 interface ContactProps {}
 
@@ -7,8 +14,35 @@ const Contact : React.FC<ContactProps> = () => {
     return (
         <ComponentWrapper id="contact">
             <SectionHeader>
-                Contact
+                Contact us
             </SectionHeader>
+            <div className={style.contactText}>
+                Jeśli masz ochotę zostać członkiem naszego Koła lub masz interesującą ofertę współpracy to zapraszamy do kontaktu mailowego, bądź przez nasze kanały social media!
+                </div>
+
+
+                <ul className={style.ul}>
+                    <li className={style.li}>
+                        <a href="">
+                            <img className={style.icon} src={facebookIcon.src} />
+                        </a>
+                    </li>
+                    <li className={style.li}>
+                        <a href="">
+                            <img className={style.icon} src={envelopeIcon.src} />
+                        </a>
+                    </li>
+                    <li className={style.li}>
+                        <a href="">
+                            <img className={style.icon} src={twitterIcon.src} />
+                        </a>
+                    </li>
+                    <li className={style.li}>
+                        <a href="">
+                            <img className={style.icon} src={linkedinIcon.src} />
+                        </a>
+                    </li>
+                </ul>
         </ComponentWrapper>
     );
 };
